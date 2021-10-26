76104bc06855a3ff10b120dbaf07204590e37ae9
Shawn Wilsher <656602+sdwilsh@users.noreply.github.com>
2021-10-26 01:01:08 -0700

    Provide a context manager for API Calls
    This provides a context manager to make working with API calls to the
    GEM device easier for callers who use asyncio.

--- tests/gem/test_api.py
+++ tests/gem/test_api.py
@@ -1,17 +1,20 @@
+from __future__ import annotations
+
 import asyncio
 import unittest
-from typing import TypeVar
+from datetime import timedelta
+from unittest.async_case import IsolatedAsyncioTestCase
+from unittest.mock import patch
 
-from siobrultech_protocols.gem.api import GET_SERIAL_NUMBER, ApiCall
+import pytest
+
+from siobrultech_protocols.gem.api import GET_SERIAL_NUMBER, ApiCall, R, T, call_api
 from siobrultech_protocols.gem.packets import Packet
 from siobrultech_protocols.gem.protocol import (
     API_RESPONSE_WAIT_TIME,
     BidirectionalProtocol,
 )
 from tests.gem.mock_transport import MockTransport
-
-T = TypeVar("T")
-R = TypeVar("R")
 
 
 class TestApi(unittest.TestCase):
@@ -48,3 +51,44 @@ class TestApi(unittest.TestCase):
         self.assertEqual(self._transport.writes, [request.encode()])
         self._protocol.data_received(encoded_response)
         self.assertEqual(call.receive_response(self._protocol), parsed_response)
+
+
+class TestContextManager(IsolatedAsyncioTestCase):
+    def setUp(self):
+        self._queue: asyncio.Queue[Packet] = asyncio.Queue()
+        self._transport = MockTransport()
+        self._protocol = BidirectionalProtocol(self._queue)
+        self._protocol.connection_made(self._transport)
+
+    @pytest.mark.asyncio
+    @patch(
+        "siobrultech_protocols.gem.protocol.API_RESPONSE_WAIT_TIME",
+        timedelta(seconds=0),
+    )
+    @patch(
+        "siobrultech_protocols.gem.protocol.PACKET_DELAY_CLEAR_TIME",
+        timedelta(seconds=0),
+    )
+    async def testApiCall(self):
+        call = ApiCall(lambda _: "REQUEST", lambda response: response)
+        async with call_api(call, self._protocol) as f:
+            self.setApiResponse("RESPONSE".encode())
+            response = await f(None)
+            self.assertEqual(response, "RESPONSE")
+
+    @pytest.mark.asyncio
+    async def testTaskCanceled(self):
+        call = ApiCall(lambda _: "REQUEST", lambda response: response)
+        with self.assertRaises(asyncio.CancelledError):
+            with patch("asyncio.sleep") as mock_sleep:
+                mock_sleep.side_effect = asyncio.CancelledError
+                async with call_api(call, self._protocol):
+                    raise AssertionError("this should not be reached")
+
+    def setApiResponse(self, ecnoded_response: bytes) -> asyncio.Task[None]:
+        async def notify_data_received() -> None:
+            self._protocol.data_received(ecnoded_response)
+
+        return asyncio.create_task(
+            notify_data_received(), name=f"{__name__}:send_api_resonse"
+        )
