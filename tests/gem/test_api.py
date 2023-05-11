19cfe2e636847a9f8974b2020053ea500cbb257a
Jonathan Keljo <jkeljo@users.noreply.github.com>
2023-05-11 14:09:53 -0700

    ECM API support
    The ECM API is rather different from GEM. Instead of a single round-trip per call, each API call involves multiple round-trips. For example, in order to set the packet send interval:
    
    1. PC sends FC (hex)
    2. ECM-1240 responds with FC (hex)
    3. PC sends three ASCII bytes “S” “E” “T” (must be upper case characters)
    4. ECM-1240 responds with FC (hex)
    5. PC sends three ASCII bytes “I” “V” “2” (must be upper case characters)
    6. ECM-1240 responds with FC (hex)
    7. PC sends a single byte chr$(interval value) sends the ascii character representing the interval.
    8. ECM-1240 responds with FC (hex)
    
    So that's 4 round-trips for one API call.
    
    This PR updates `ApiCall` to allow separate formatters and parsers to be supplied for GEM and ECM, adds the necessary behaviors in `BidirectionalProtocol`, and supplies implementations for serial number and packet send frequency (the two GEM APIs we support that have ECM equivalents).

--- tests/gem/test_api.py
+++ tests/gem/test_api.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import asyncio
 from datetime import datetime, timedelta
-from typing import Optional
+from typing import List, Optional
 from unittest.async_case import IsolatedAsyncioTestCase
 from unittest.mock import patch
 
@@ -25,6 +25,7 @@ from siobrultech_protocols.gem.api import (
 )
 from siobrultech_protocols.gem.packets import PacketFormatType
 from siobrultech_protocols.gem.protocol import (
+    ApiType,
     BidirectionalProtocol,
     PacketProtocolMessage,
 )
@@ -36,43 +37,39 @@ class TestApi(IsolatedAsyncioTestCase):
         self._queue: asyncio.Queue[PacketProtocolMessage] = asyncio.Queue()
         self._transport = MockTransport()
         self._protocol = BidirectionalProtocol(
-            self._queue, packet_delay_clear_time=timedelta(seconds=0)
+            self._queue,
+            packet_delay_clear_time=timedelta(seconds=0),
+            api_type=ApiType.GEM,
         )
         self._protocol.connection_made(self._transport)
 
-        # Put the protocol into a state where it's ready for commands
-        # and we can see exactly what is sent
-        self._protocol.begin_api_request()
-        self._transport.writes.clear()
-
     async def testApiCallWithoutResponse(self):
-        call = ApiCall(lambda _: "REQUEST", None)
+        call = ApiCall(lambda _: "REQUEST", None, None, None)
 
         await self.assertCall(call, "REQUEST", None, None, None, None)
 
     async def testApiCall(self):
-        call = ApiCall(lambda _: "REQUEST", lambda response: response)
+        call = ApiCall(lambda _: "REQUEST", lambda response: response, None, None)
 
         await self.assertCall(
             call, "REQUEST", None, None, "RESPONSE".encode(), "RESPONSE"
         )
 
     async def testApiCallWithSerialNumber(self):
-        call = ApiCall(lambda _: "^^^REQUEST", lambda response: response)
+        call = ApiCall(lambda _: "^^^REQUEST", lambda response: response, None, None)
 
         await self.assertCall(
             call, "^^^NMB02345REQUEST", None, 1002345, "RESPONSE".encode(), "RESPONSE"
         )
 
     async def testApiCallIgnored(self):
-        call = ApiCall(lambda _: "REQUEST", lambda response: response)
+        call = ApiCall(lambda _: "REQUEST", lambda response: response, None, None)
 
-        self._protocol.end_api_request()
         async with call_api(call, self._protocol, timeout=timedelta(seconds=0)) as f:
             with self.assertRaises(asyncio.exceptions.TimeoutError):
                 await f(None)
 
-    async def testGetSerialNumber(self):
+    async def testGEMGetSerialNumber(self):
         await self.assertCall(
             GET_SERIAL_NUMBER,
             "^^^RQSSRN",
@@ -80,6 +77,16 @@ class TestApi(IsolatedAsyncioTestCase):
             None,
             "1234567\r\n".encode(),
             1234567,
+        )
+
+    async def testECMGetSerialNumber(self):
+        await self.assertECMCall(
+            GET_SERIAL_NUMBER,
+            [b"\xfc", b"SET", b"RCV"],
+            None,
+            None,
+            b"\xa7\x04\xa7\x04\xf4\x03\x01\x0c\x04\x0b\x05\x01\xb2\x90\x00\x00\x00\x00\x8d\x8b\x8c\x8b\xce\x00\xff\xff\xff\xff\x05\x80>\x00m",
+            545569,
         )
 
     async def testSetDateTime(self):
@@ -102,7 +109,7 @@ class TestApi(IsolatedAsyncioTestCase):
             True,
         )
 
-    async def testSetPacketSendInterval(self):
+    async def testGEMSetPacketSendInterval(self):
         await self.assertCall(
             SET_PACKET_SEND_INTERVAL,
             "^^^SYSIVL042",
@@ -110,6 +117,16 @@ class TestApi(IsolatedAsyncioTestCase):
             None,
             "IVL\r\n".encode(),
             True,
+        )
+
+    async def testECMSetPacketSendInterval(self):
+        await self.assertECMCall(
+            SET_PACKET_SEND_INTERVAL,
+            [b"\xfc", b"SET", b"IV2", bytes([42])],
+            42,
+            None,
+            None,
+            None,
         )
 
     async def testSetSecondaryPacketFormat(self):
@@ -131,6 +148,8 @@ class TestApi(IsolatedAsyncioTestCase):
         encoded_response: bytes | None,
         parsed_response: R | None,
     ):
+        self._protocol.begin_api_request()
+        self._transport.writes.clear()  # Ignore the packet delay
         result = asyncio.get_event_loop().create_future()
         self._protocol.invoke_api(call, arg, result, serial_number)
         self.assertEqual(
@@ -147,13 +166,53 @@ class TestApi(IsolatedAsyncioTestCase):
             f"{parsed_response} should be the parsed value returned",
         )
 
+    async def assertECMCall(
+        self,
+        call: ApiCall[T, R],
+        request: List[bytes],
+        arg: T,
+        serial_number: Optional[int],
+        encoded_response: bytes | None,
+        parsed_response: R | None,
+    ):
+        self._protocol.api_type = ApiType.ECM
+        self._transport.writes.clear()
+        self._protocol.begin_api_request()
+        result = asyncio.get_event_loop().create_future()
+        self._protocol.invoke_api(call, arg, result, serial_number)
+        for _ in range(0, len(request)):
+            self._protocol.data_received(b"\xfc")
+        self.assertEqual(
+            self._transport.writes,
+            request,
+            f"{request} should be written to the transport",
+        )
+        if encoded_response is not None:
+            self._transport.writes.clear()
+            self._protocol.data_received(encoded_response)
+        result = await asyncio.wait_for(result, 0)
+        self.assertEqual(
+            result,
+            parsed_response,
+            f"{parsed_response} should be the parsed value returned",
+        )
+        self._protocol.end_api_request()
+        if encoded_response is not None:
+            self.assertEqual(
+                self._transport.writes,
+                [b"\xfc"],
+                f"ECM API calls with a response should be acked by the caller",
+            )
+
 
 class TestContextManager(IsolatedAsyncioTestCase):
     def setUp(self):
         self._queue: asyncio.Queue[PacketProtocolMessage] = asyncio.Queue()
         self._transport = MockTransport()
         self._protocol = BidirectionalProtocol(
-            self._queue, packet_delay_clear_time=timedelta(seconds=0)
+            self._queue,
+            packet_delay_clear_time=timedelta(seconds=0),
+            api_type=ApiType.GEM,
         )
         self._protocol.connection_made(self._transport)
 
@@ -162,14 +221,14 @@ class TestContextManager(IsolatedAsyncioTestCase):
         timedelta(seconds=0),
     )
     async def testApiCall(self):
-        call = ApiCall(lambda _: "REQUEST", lambda response: response)
+        call = ApiCall(lambda _: "REQUEST", lambda response: response, None, None)
         async with call_api(call, self._protocol) as f:
             self.setApiResponse("RESPONSE".encode())
             response = await f(None)
             self.assertEqual(response, "RESPONSE")
 
     async def testTaskCanceled(self):
-        call = ApiCall(lambda _: "REQUEST", lambda response: response)
+        call = ApiCall(lambda _: "REQUEST", lambda response: response, None, None)
         with self.assertRaises(asyncio.CancelledError):
             with patch("asyncio.sleep") as mock_sleep:
                 mock_sleep.side_effect = asyncio.CancelledError
@@ -188,7 +247,9 @@ class TestContextManager(IsolatedAsyncioTestCase):
 class TestApiHelpers(IsolatedAsyncioTestCase):
     def setUp(self):
         self._protocol = BidirectionalProtocol(
-            asyncio.Queue(), packet_delay_clear_time=timedelta(seconds=0)
+            asyncio.Queue(),
+            packet_delay_clear_time=timedelta(seconds=0),
+            api_type=ApiType.GEM,
         )
 
         patcher_API_RESPONSE_WAIT_TIME = patch(
